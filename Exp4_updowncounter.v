66a8b0ceafba63684cfe3c6e195ea28257b5ad9f
hindujha-vijayakumar <hindujhavijayakumar@gmail.com>
2024-05-16 19:12:41 +0530

    Update Exp4_updowncounter.v

--- Exp4_updowncounter.v
+++ Exp4_updowncounter.v
@@ -1,4 +1,4 @@
-module updown(clk,rst,count);
+module up(clk,rst,count);
 input clk,rst;
 output[3:0]count;
 reg[3:0]count;
@@ -8,5 +8,19 @@ if(rst)
 count<=4'b0;
 else
 count<=count+1;
+end
+endmodule
+
+
+module downcounter(clk,rst,count);
+input clk,rst;
+output[3:0]count;
+reg[3:0]count;
+always@(posedge clk)
+begin
+if(rst)
+count<=4'b0;
+else
+count <=count-1;
 end
 endmodule
